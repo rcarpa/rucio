b7e27abae66c01d72ee84bb9f305b45710e974d4
Radu Carpa <radu.carpa@cern.ch>
2021-04-20 16:40:22 +0200

    Core & Internals: substitute raw epoch with OBSOLETE constant

--- lib/rucio/core/transfer.py
+++ lib/rucio/core/transfer.py
@@ -70,7 +70,7 @@ from rucio.core.request import queue_requests, set_requests_state
 from rucio.core.rse import get_rse_name, get_rse_vo, list_rses, get_rse_supported_checksums
 from rucio.core.rse_expression_parser import parse_expression
 from rucio.db.sqla import models, filter_thread_work
-from rucio.db.sqla.constants import DIDType, RequestState, RSEType, RequestType, ReplicaState
+from rucio.db.sqla.constants import DIDType, RequestState, RSEType, RequestType, ReplicaState, OBSOLETE
 from rucio.db.sqla.session import read_session, transactional_session
 from rucio.rse import rsemanager as rsemgr
 from rucio.transfertool.fts3 import FTS3Transfertool
@@ -1135,7 +1135,7 @@ def get_transfer_requests_and_source_replicas(total_workers=0, worker_number=0, 
                               'bytes': transfers[req_id]['file_metadata']['filesize'],
                               'adler32': transfers[req_id]['file_metadata']['adler32'],
                               'md5': transfers[req_id]['file_metadata']['md5'],
-                              'tombstone': tombstone_from_delay(ctx.rse_attrs(dest_rse_id).get('multihop_tombstone_delay')) or datetime.datetime(year=1970, month=1, day=1),
+                              'tombstone': tombstone_from_delay(ctx.rse_attrs(dest_rse_id).get('multihop_tombstone_delay')) or OBSOLETE,
                               'state': 'C'}]
                     try:
                         add_replicas(rse_id=hop['dest_rse_id'],
